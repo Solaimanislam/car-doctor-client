33ccf306b204598dd9a843f0255c10134755c5be
Solaimanislam <islamsi2001@gmail.com>
2024-05-04 11:59:59 +0600

    added login & signUp

--- src/Shared/NavBar/NavBar.jsx
+++ src/Shared/NavBar/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
     </>
 
     return (
-        <div className="navbar bg-base-100 h-28">
+        <div className="navbar bg-base-100 lg:h-28 mb-8">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
